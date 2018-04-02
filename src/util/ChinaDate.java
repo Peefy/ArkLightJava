b79fbf2b47308e0923701273245bdb89b4b48a92
Peefy <xpf6677@163.com>
2018-04-02 16:42:53 +0800

    Initial commit

---
+++ src/util/ChinaDate.java
@@ -0,0 +1,84 @@
+
+public class ChinaDate{
+        /*
+         * 农历年(整型)
+         */
+        private int cnIntYear = 0;
+        /// <summary>
+        /// 农历月份(整型)
+        /// </summary>
+        private int cnIntMonth = 0;
+        /// <summary>
+        /// 农历天(整型)
+        /// </summary>
+        private int cnIntDay = 0;
+        /// <summary>
+        /// 农历年(支干)
+        /// </summary>
+        private string cnStrYear = "";
+        /// <summary>
+        /// 农历月份(字符)
+        /// </summary>
+        private string cnStrMonth = "";
+        /// <summary>
+        /// 农历天(字符)
+        /// </summary>
+        private string cnStrDay = "";
+        /// <summary>
+        /// 农历属象
+        /// </summary>
+        private string cnAnm = "";
+        /// <summary>
+        /// 二十四节气
+        /// </summary>
+        private string cnSolarTerm = "";
+        /// <summary>
+        /// 阴历节日
+        /// </summary>
+        private string cnFtvl = "";
+        /// <summary>
+        /// 阳历节日
+        /// </summary>
+        private string cnFtvs = "";
+        /// <summary>
+        /// 农历年(整型)
+        /// </summary>
+        public int CnIntYear { get => cnIntYear; set => cnIntYear = value; }
+        /// <summary>
+        /// 农历月份(整型)
+        /// </summary>
+        public int CnIntMonth { get => cnIntMonth; set => cnIntMonth = value; }
+        /// <summary>
+        /// 农历天(整型)
+        /// </summary>
+        public int CnIntDay { get => cnIntDay; set => cnIntDay = value; }
+        /// <summary>
+        /// 农历年(支干)
+        /// </summary>
+        public string CnStrYear { get => cnStrYear; set => cnStrYear = value; }
+        /// <summary>
+        /// 农历月份(字符)
+        /// </summary>
+        public string CnStrMonth { get => cnStrMonth; set => cnStrMonth = value; }
+        /// <summary>
+        /// 农历天(字符)
+        /// </summary>
+        public string CnStrDay { get => cnStrDay; set => cnStrDay = value; }
+        /// <summary>
+        /// 农历属象
+        /// </summary>
+        public string CnAnm { get => cnAnm; set => cnAnm = value; }
+        /// <summary>
+        /// 二十四节气
+        /// </summary>
+        public string CnSolarTerm { get => cnSolarTerm; set => cnSolarTerm = value; }
+        /// <summary>
+        /// 阴历节日
+        /// </summary>
+        public string CnFtvl { get => cnFtvl; set => cnFtvl = value; }
+        /// <summary>
+        /// 阳历节日
+        /// </summary>
+        public string CnFtvs { get => cnFtvs; set => cnFtvs = value; }
+}
+
---
+++ test/test.java
@@ -0,0 +1,11 @@
+
+import java.lang.*;
+import java.lang.System;
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("hello test function!");
+    }
+}
+
+
